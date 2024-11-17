7287f6f97a308ce1cccd59126a254b7b8dda5283
sudesh <sudeshyalavarthi@gmail.com>
2024-11-17 16:15:45 +0000

    new ips added to the repo

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TunnelBearIPs
+## Overview
+
+This repository contains a list of IP addresses used by TunnelBear VPN. These IPs are collected and updated regularly to ensure they are current and accurate.
+
+## Usage
+
+You can use this list to configure your firewall, proxy, or any other network-related tools to allow or block TunnelBear VPN traffic.
+
+## Contributing
+
+If you have new IP addresses to add or notice any discrepancies, please open an issue or submit a pull request.
+
+
+
+## Contact
+
+For any questions or concerns, please contact the repository owner.
--- tunnelbear.py
+++ tunnelbear.py
@@ -1,10 +1,12 @@
 import dns.resolver
 import os
 import requests
+import csv
 
 DNS_SERVERS = ['8.8.8.8', '8.8.4.4']
 DOMAIN_FILE = 'tunnerlbear.txt'
 OUTPUT_FILE = 'tunnelbear_subnet.txt'
+OUTPUT_CSV_FILE = "tunnebear_ips.csv"
 IP_GUIDE_URL = "https://ip.guide/"
 
 def configure_dns_resolver():
@@ -34,34 +36,46 @@ def fetch_subnet_for_ip(ip):
         subnet = f"{'.'.join(ip_split[:3])}.0/24"
     return subnet
 
-def resolve_domains_to_subnets(domains, resolver):
+def resolve_domains(domains, resolver, resolve_subnets=False):
+    ip_hostnames = {}
     ip_subnet = set()
     for domain in domains:
         print(f"Resolving domain: {domain}")
         try:
-            temp_ips=set()
-            for i in range(0,10):
+            temp_ips = set()
+            for _ in range(10):
                 answers = resolver.resolve(domain, 'A')
                 for ip in answers:
                     temp_ips.add(str(ip))
-            for ip in temp_ips:
-                subnet = fetch_subnet_for_ip(ip)
-                print(f"Subnet: {subnet}")
-                ip_subnet.add(subnet)
+            ip_hostnames[domain] = temp_ips
+            if resolve_subnets:
+                for ip in temp_ips:
+                    subnet = fetch_subnet_for_ip(ip)
+                    print(f"Subnet: {subnet}")
+                    ip_subnet.add(subnet)
         except Exception as e:
             print(f"Error resolving {domain}: {e}")
-    return ip_subnet
+    return ip_hostnames, ip_subnet
 
-def write_subnets_to_file(subnets, file_path):
-    with open(file_path, 'w') as f:
-        for item in subnets:
-            f.write(f"{item}\n")
+def write_to_file(data, file_path, mode='w'):
+    with open(file_path, mode) as f:
+        if isinstance(data, set):
+            for item in data:
+                f.write(f"{item}\n")
+        elif isinstance(data, dict):
+            fieldnames = ['Hostname', 'IP']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for hostname, ips in data.items():
+                for ip in ips:
+                    writer.writerow({'Hostname': hostname, 'IP': ip})
 
 def main():
     resolver = configure_dns_resolver()
     domains = read_domains_from_file(DOMAIN_FILE)
-    subnets = resolve_domains_to_subnets(domains, resolver)
-    write_subnets_to_file(subnets, OUTPUT_FILE)
+    ip_hostnames, subnets = resolve_domains(domains, resolver, resolve_subnets=True)
+    write_to_file(ip_hostnames, OUTPUT_CSV_FILE)
+    write_to_file(subnets, OUTPUT_FILE)
 
 if __name__ == "__main__":
     main()
